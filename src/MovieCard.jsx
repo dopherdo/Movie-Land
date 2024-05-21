8acda99176884912553cdbf828476e398a5e4c07
Chris Yeh <dopherdo@users.noreply.github.com>
2024-05-21 16:17:38 -0700

    Added button without the functionality to make a watchlist

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const addToWatchlist = () => {
+    // Implement the functionality to add the movie to the watchlist
+    console.log('Added to watchlist:', Title);
+  };
+
+
   return (
     <div className="movie" key={imdbID}>
       <div>
         <p>{Year}</p>
+        <button className="watchlist-btn" onClick={addToWatchlist}>Watch Later</button>
       </div>
+      
 
       <div>
         <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
